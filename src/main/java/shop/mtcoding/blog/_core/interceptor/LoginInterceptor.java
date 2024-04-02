0e17db0f263f5536aae868f3b41925c42e830aba
yeeun0118s <yeeun0118s@naver.com>
2024-04-02 14:22:52 +0900

    JWT로 로그인

--- src/main/java/shop/mtcoding/blog/_core/interceptor/LoginInterceptor.java
+++ src/main/java/shop/mtcoding/blog/_core/interceptor/LoginInterceptor.java
@@ -1,24 +1,46 @@
 package shop.mtcoding.blog._core.interceptor;
 
+import com.auth0.jwt.exceptions.JWTDecodeException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import shop.mtcoding.blog._core.errors.exception.Exception400;
 import shop.mtcoding.blog._core.errors.exception.Exception401;
+import shop.mtcoding.blog._core.errors.exception.Exception500;
+import shop.mtcoding.blog._core.utils.Jwtutill;
+import shop.mtcoding.blog.user.SessionUser;
 import shop.mtcoding.blog.user.User;
 
 // /api/** 인증 필요 주소
 public class LoginInterceptor implements HandlerInterceptor{
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("preHandle............");
-        HttpSession session = request.getSession();
 
-        User sessionUser = (User) session.getAttribute("sessionUser");
-        if(sessionUser == null){
-            throw new Exception401("로그인 하셔야 해요");
+        //Bearer jwt토근
+        String  jwt = request.getHeader("Autorization");
+
+        if (jwt == null){ //필터에서 하면 어렵다  그래서 인터셉터에서 한다.
+            throw new Exception401("jwt 토큰을 전달해주세요");
         }
-        return true;
+
+        jwt = jwt.replace("Bearer ", "");
+
+
+
+        //검증
+        try {
+            SessionUser sessionUser = Jwtutill.verify(jwt);
+
+            //임시 세션 (jessionId는 필요 없다)
+            HttpSession session = request.getSession();
+            session.setAttribute("sessionUser",sessionUser);
+            return  true;
+        }catch (JWTDecodeException e){
+            throw new Exception401("토큰이 유효하지 않습니다");
+        }catch (Exception e){
+            throw new Exception500(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/shop/mtcoding/blog/_core/utils/Jwtutill.java
@@ -0,0 +1,35 @@
+package shop.mtcoding.blog._core.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import shop.mtcoding.blog.user.SessionUser;
+import shop.mtcoding.blog.user.User;
+
+import java.util.Date;
+
+public class Jwtutill {
+
+    public static String create(User user){ //토근 생성
+
+        String jwt = JWT.create()
+                .withSubject("blog")
+                .withExpiresAt(new Date(System.currentTimeMillis()+ 1000*60*60))
+                .withClaim("id",user.getId())
+                .withClaim("username", user.getUsername())
+                .sign(Algorithm.HMAC512("metacoding"));
+
+        return jwt;
+    }
+
+    public static SessionUser verify(String jwt){ //토근 검증 (임시 세션)
+        DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512("metacoding")).build().verify(jwt);
+        int id = decodedJWT.getClaim("id").asInt();
+        String username = decodedJWT.getClaim("username").asString();
+
+        return SessionUser.builder()
+                .id(id)
+                .username(username)
+                .build();
+    }
+}
